60d6a0d2635178a473c30b895b71d0a4ee24887c
KevDev <ing.kevinlopezsistemas@gmail.com>
2016-12-06 14:47:13 -0600

    add listview and items

--- app/src/main/java/com/example/kevdev/aurora/Controller/ActivityPrincipal.java
+++ app/src/main/java/com/example/kevdev/aurora/Controller/ActivityPrincipal.java
@@ -3,27 +3,45 @@ package com.example.kevdev.aurora.Controller;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.kevdev.aurora.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by KevDev on 06/12/16.
  */
 public class ActivityPrincipal extends AppCompatActivity {
     Toolbar toolbar;
-
+    ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
+        List<String> items = new ArrayList<>();
         toolbar = (Toolbar)findViewById(R.id.toolbar);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setActionBar(toolbar);
-        }
+        setSupportActionBar(toolbar);
+
+        lista = (ListView) findViewById(R.id.listViewSongs);
+
+        items.add("Pop");
+        items.add("Rock");
+        items.add("Electronica");
+        items.add("Banda");
+        items.add("Blues");
+        items.add("Hip-hop");
+        items.add("Reaggue");
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+
+        lista.setAdapter(adapter);
+
 
     }
 
