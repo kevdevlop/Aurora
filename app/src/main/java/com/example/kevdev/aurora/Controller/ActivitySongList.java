93ab7f7d5dba2b151031f748a357691bae7f9c35
KevDev <ing.kevinlopezsistemas@gmail.com>
2017-03-15 13:56:28 -0600

    others

--- app/src/main/java/com/example/kevdev/aurora/Controller/ActivitySongList.java
+++ app/src/main/java/com/example/kevdev/aurora/Controller/ActivitySongList.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,9 +17,11 @@ import android.widget.Toast;
 
 import com.example.kevdev.aurora.Adapters.AdapterPlaylist;
 import com.example.kevdev.aurora.Adapters.AdapterSongsPlayList;
+import com.example.kevdev.aurora.Adapters.PlayerAdapter;
 import com.example.kevdev.aurora.MainActivity;
 import com.example.kevdev.aurora.Model.SongModel;
 import com.example.kevdev.aurora.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -48,90 +51,77 @@ public class ActivitySongList extends AppCompatActivity {
         Intent in = getIntent();
         Bundle b = in.getExtras();
 
-
-////////////////////////////////////////////////////////////////////
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Intent i = null;
 
-                switch (item.getItemId()) {
-                    case R.id.profile:
-                        Toast.makeText(ActivitySongList.this, "Ir a perfil de usuario", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.playList:
-                        i = new Intent(ActivitySongList.this,ActivityPlaylist.class);
-                        startActivity(i);
-                        break;
-                    case  R.id.custom:
-                        Toast.makeText(ActivitySongList.this, "Ir a perfil de settings", Toast.LENGTH_LONG).show();
-                        break;
-                    case R.id.logout:
-                        ActivitySongList.this.finish();
-                        i = new Intent(ActivitySongList.this, MainActivity.class);
-                        startActivity(i);
-                        break;
-
-                }
-
-                return true;
-            }
-        });
-        toolbar.inflateMenu(R.menu.main);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-        AdapterSongsPlayList adapterPlaylist;
+        PlayerAdapter adapterPlaylist;
         final List<SongModel> songsList = new ArrayList();
         lista = (ListView) findViewById(R.id.listViewSongs);
         nombrePLay = (TextView) findViewById(R.id.nombrePLay);
-        //Drawable d;
-        nombrePLay.setText(b.getString("Genero"));
-       /* Bitmap bmp = null;
-        URL url = null;
-        try {
-            url = new URL("https://firebasestorage.googleapis.com/v0/b/aurora-c5519.appspot.com/o/Songs%2Fimagen%2F1.jpg?alt=media&token=80ed7bb0-2689-4ce4-bf5f-9ff2831f7794");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        try {
-            bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
-        int genero = b.getInt("Posicion");
-        for (int i=0; i<1; i++){    //necesito doble ciclo para crear lista de cancones
-            DatabaseReference mensajeRef = ref.child(genero+"/albums/"+i+"/songs/0/title");
+        //se establece el nombre de la playlist que se selecciono
+        nombrePLay.setText(b.getString("namePlaylist"));
 
-            mensajeRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    String l= dataSnapshot.getValue(String.class);
-                    songsList.add(new SongModel(l,"Rvssian ft. Arcangel, Nicky Jam, Farruko, Konshens", "Trap", "Rvssian" ));
-                }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
 
-                }
-            });
-
-        }
 
         //imageView.setImageBitmap(bmp);
-        //songsList.add(new SongModel("Privado","Rvssian ft. Arcangel, Nicky Jam, Farruko, Konshens", "Trap", "Rvssian" ));
-        //songsList.add(new SongModel("Enter Sadman","Metallica", "Rock", "Metallica"));
+        songsList.add(new SongModel("Privado","Rvssian ft. Arcangel, Nicky Jam, Farruko, Konshens", "Trap", "Rvssian" ));
+        songsList.add(new SongModel("Enter Sadman","Metallica", "Rock", "Metallica"));
        /* songsList.add(new SongModel("That was just your life","Metallica", "Rock", "Death Magnetic",R.drawable.dead));
         songsList.add(new SongModel("Frantic","Metallica", "Rock", "ST. Anger",R.drawable.metallica));
         songsList.add(new SongModel("Batery","Metallica", "Rock", "Master of puppets",R.drawable.masterofpuppets));
         songsList.add(new SongModel("Fade to Black","Metallica", "Rock", "Ride the Ligthing",R.drawable.ride));**/
 
-        adapterPlaylist = new AdapterSongsPlayList(songsList,this);
+        adapterPlaylist = new PlayerAdapter(this,songsList);
         lista.setAdapter(adapterPlaylist);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SongModel song = (SongModel) lista.getItemAtPosition(position);
+
+                Intent i = new Intent(ActivitySongList.this, ActivityReproductor.class);
+                i.putExtra("nombre", song.getNombre());
+                i.putExtra("artista", song.getArtista());
+                //  i.putExtra("imagen", song.getImagen());
+                startActivity(i);
+            }
+        });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        /*MenuItem item = menu.getItem(R.id.profile);
+        item.setTitle(FirebaseAuth.getInstance().getCurrentUser().getEmail());*/
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                this.finish();
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.profile:
+                Intent ip = new Intent(this, ActivityProfile.class);
+                startActivity(ip);
+                return true;
+            case R.id.playList:
+                Intent i2 = new Intent(this,ActivityPlaylist.class);
+                startActivity(i2);
+                return true;
+            case R.id.custom:
+                Toast.makeText(this, "custom", Toast.LENGTH_LONG ).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
